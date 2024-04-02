0757d0934963f8ff49fdb93b6e03f16059a9d2ff
ashritha <ashritha.achala90@gmail.com>
2024-04-02 18:06:42 -0400

    testing env variables

--- expence.v1/ec2.tf
+++ expence.v1/ec2.tf
@@ -1,26 +1,38 @@
 resource "aws_instance" "frontend"{
-  ami = "ami-05f020f5935e52dc4"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-00c2949e503e295c2"]
+  ami = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
-    Name = "frontend"
+  Name = "frontend"
   }
 }
 
 resource "aws_instance" "backend"{
-  ami = "ami-05f020f5935e52dc4"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-00c2949e503e295c2"]
+  ami = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
-    Name = "backend"
+  Name = "backend"
   }
 }
 
 resource "aws_instance" "mysql"{
-  ami = "ami-05f020f5935e52dc4"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-00c2949e503e295c2"]
+  ami = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
   tags = {
-    Name = "mysql"
+  Name = "mysql"
   }
+}
+
+variable "ami" {
+  default = "ami-05f020f5935e52dc4"
+}
+
+variable "instance_type" {
+  default = " t3.micro"
+}
+
+variable "security_group" {
+  default = ["sg-00c2949e503e295c2"]
 }
