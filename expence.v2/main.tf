d2782800213a00117e1214f80e3b259ff77db5a2
ashritha <ashritha.achala90@gmail.com>
2024-04-03 11:25:44 -0400

    testing env variables

--- expence.v2/main.tf
+++ expence.v2/main.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "frontend"{
+resource "aws_instance" "frontend" {
   for_each      = var.instance_types
   ami           = var.ami
-  instance_type = each.value ["instance_types"]
+  instance_type = each.value ["instance_type"]
   vpc_security_group_ids = var.security_group
   tags = {
     Name = "each.key"
@@ -10,7 +10,9 @@ resource "aws_instance" "frontend"{
 variable "ami" {
   default = "ami-05f020f5935e52dc4"
 }
-
+variable "instance_type" {
+  default = "t3.micro"
+}
 variable "security_group" {
   default = ["sg-00c2949e503e295c2"]
 }
